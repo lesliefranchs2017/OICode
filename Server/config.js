9ae564604d805acc5a6b013636eea7b19262811f
GainesAndrew <31869886+GainesAndrew@users.noreply.github.com>
2019-10-08 13:03:41 -0600

    pushing the server

---
+++ Server/config.js
@@ -0,0 +1,12 @@
+const config = {
+
+    db: {
+        user:"zdgylsqckpfeuk",
+        host:"ec2-54-83-9-169.compute-1.amazonaws.com",
+        database:"d7b9m4tbdo4u6m",
+        password: "silverfox",
+        port:"5432",
+        ssl:true   
+    }
+   };
+   module.exports = config;
