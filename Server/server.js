fd1e06cb4312c82248a98dedb81e6e8696502034
Andrew Gaines <31869886+GainesAndrew@users.noreply.github.com>
2019-10-10 09:00:08 -0600

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var path = require('path'); 
+var path = require('path');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
 const pg = require('pg');
@@ -8,11 +8,12 @@ var sequelize = require('sequelize');
 var client = new pg.Client();
 const config = require('./config');
 const { performance } = require('perf_hooks');
+
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 
-//enabling css style sheet 
+//enabling css style sheet
 app.use(express.static(__dirname + '/public'));
 
 var configs = {
@@ -21,7 +22,7 @@ var configs = {
     database:config.db.database,
     password:config.db.password,
     port:config.db.port,
-    ssl:config.db.ssl   
+    ssl:config.db.ssl
 }
 
 // initializes a connection pool, keps idle connection open for 30 seconds, max 10 idle clients
@@ -53,12 +54,11 @@ app.use(bodyParser.text({ type: 'text/xml' }))
 
 function primary_key_generator(){
     return (
-      Number(String(Math.random()).slice(2)) + 
-      Date.now() + 
+      Number(String(Math.random()).slice(2)) +
+      Date.now() +
       Math.round(performance.now())
     ).toString(36);
 }
-
 
     //recieve request from the front end and store it into the database
 app.post('/add_client' , function(req,res){
@@ -69,17 +69,29 @@ app.post('/add_client' , function(req,res){
     var phone_input = req.body.phone;
     var phoneType_input= req.body.phoneType;
     var addressOne_input = req.body.addressOne;
-    var addressTwo_input = req.body.addressTwo;    
+    var addressTwo_input = req.body.addressTwo;
     var city_input= req.body.city;
     var state_input = req.body.state;
     var zip_input = req.body.zip;
     var county_input = req.body.county;
     var startDate_input = req.body.startDate;
     var clientId_input = primary_key_generator();
-    var companyStatus_input = true;
-    var date = "24/09/2018";
+    var companyStatus_input = req.body.toggle;
+    var companyStatus = false;
+	
+    // checking if toggle switch is on or off
+    if( companyStatus_input == "on" )
+    {
+        companyStatus = true;
+    }
+
+    else 
+    {
+        companyStatus = false;
+    }
+
+    // reversing the date for postgreSQL 
     startDate_input = startDate_input.split("/").reverse().join("-");
-    console.log(startDate_input);
 
 const client = new Client({
     user:config.db.user,
@@ -87,38 +99,34 @@ const client = new Client({
     database:config.db.database,
     password:config.db.password,
     port:config.db.port,
-    ssl:config.db.ssl  
+    ssl:config.db.ssl
   })
 
-  
 client.connect()
 
 const insertText = 'INSERT INTO client_table (client_id, name, company, email, phone, phone_type, address_one,\
                     address_two, city, state, zip, county, start_date, company_status) VALUES ($1, $2, $3, $4, $5, $6,\
                     $7, $8, $9, $10, $11, $12, $13, $14)'
-            
-client.query(insertText, [clientId_input, clientName_input, company_input, email_input, phone_input, phoneType_input, 
-addressOne_input, addressTwo_input, city_input, state_input, zip_input, county_input, startDate_input, companyStatus_input],(err,res)=>{
 
-    if (err) 
+client.query(insertText, [clientId_input, clientName_input, company_input, email_input, phone_input, phoneType_input,
+addressOne_input, addressTwo_input, city_input, state_input, zip_input, county_input, startDate_input, companyStatus],(err,res)=>{
+
+    if (err)
     {
         console.log(err);
         client.end();
-        res.status(400).send(err);
+        //res.status(400).send(err);
     }
     else{
         console.log(err,res)
-        console.log("DATA was succesfully inputed into database ");//+ JSON.stringify(data) );    
-       client.end();
+        console.log("DATA was succesfully inputed into database ");//+ JSON.stringify(data) );
+        client.end();
     }
 })
-  
+
 	res.set({
 		'Access-Control-Allow-Origin' : '*'
 	});
-	return res.redirect('/public/success.html');  
-
-
+	return res.redirect('/public/success.html');
 });
-
 
