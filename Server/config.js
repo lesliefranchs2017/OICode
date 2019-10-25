4afc725af3e1dbd40c15914df34f691281a38922
Andrew Gaines <31869886+GainesAndrew@users.noreply.github.com>
2019-10-25 16:57:52 -0600

    Update config.js

--- Server/config.js
+++ Server/config.js
@@ -1,10 +1,10 @@
 const config = {
 
     db: {
-        user:"zdgylsqckpfeuk",
-        host:"ec2-54-83-9-169.compute-1.amazonaws.com",
-        database:"d7b9m4tbdo4u6m",
-        password: "silverfox",
+        user:"vtqkwzicvcuzdi",
+        host:"ec2-107-20-234-175.compute-1.amazonaws.com",
+        database:"dc0n69nsjcts2p",
+        password: "d943d74a98c8b558fd4e3f3a78567be184e2752026726bb5de4828a235dfe6d0",
         port:"5432",
         ssl:true   
     }
