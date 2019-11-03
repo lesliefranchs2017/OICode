1983ff2e1d30e3fd8f690e2c8a80a590050010b2
toririggs <vlriggs@mavs.coloradomesa.edu>
2019-11-03 16:33:38 -0700

    update

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+
+    db: {
+        user:"vtqkwzicvcuzdi",
+        host:"ec2-107-20-234-175.compute-1.amazonaws.com",
+        database:"dc0n69nsjcts2p",
+        password: "d943d74a98c8b558fd4e3f3a78567be184e2752026726bb5de4828a235dfe6d0",
+        port:"5432",
+        ssl:true   
+    }
+   };
+   module.exports = config;
